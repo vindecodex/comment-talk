a1b60748e2204acbd3ffdd911882e50e49662d2b
Vincent Villaluna <villalunavincent@gmail.com>
2020-12-21 14:38:13 +0800

    :tada: init client

--- client/client.js
+++ client/client.js
@@ -1,14 +1,18 @@
-const io = require('socket.io-client');
+const { Manager } = require("socket.io-client");
 
-const socket = io('http://locallhost:1818');
+const manager = new Manager("ws://localhost:1818");
+const socket = manager.socket("/");
 
-socket.on('connect', () => {
-	console.log(socket.connected);
-})
+socket.on("connect", () => {
+  console.log(`connect ${socket.id}`);
+});
+
+socket.on("disconnect", () => {
+  console.log(`disconnect`);
+});
 
 setInterval(() => {
-	console.log("A")
-	socket.emit("ping", () => {
-		console.log("pong");
-	})
-}, 2000);
+  socket.emit("ping", () => {
+    console.log("pong");
+  });
+}, 1000);

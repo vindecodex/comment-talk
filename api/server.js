52a946949807618cfd3540d8f271541f756181db
Vincent Villaluna <villalunavincent@gmail.com>
2020-12-22 11:31:45 +0800

    :rocket: fix response

--- api/server.js
+++ api/server.js
@@ -1,14 +1,20 @@
 const app = require('express')();
 const Chats = require('./data.js');
 
+app.use((req,res,next) => {
+	res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+})
+
 app.get('/', (req,res) => {
 	console.log('Welcome to API');
 	res.send('Welcome to API');
 })
 
 app.get('/chats', (req,res) => {
-	console.log(Chats);
-	res.status(200).json(Chats);
+	console.log(Chats.Chats);
+	res.status(200).json(Chats.Chats);
 })
 
 app.listen(9001,() => {

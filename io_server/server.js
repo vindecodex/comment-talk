e0e73d490104a89e47baf025aacbd1ed7ad8415e
Vincent Villaluna <villalunavincent@gmail.com>
2020-12-22 10:08:42 +0800

    :tada: fix dir

---
+++ io_server/server.js
@@ -0,0 +1,16 @@
+const { Server } = require('socket.io');
+
+const io = new Server(1818);
+
+io.on("connect", (socket) => {
+  console.log(`connect ${socket.id}`);
+
+  socket.on("ping", (cb) => {
+    console.log("ping");
+    cb();
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`disconnect ${socket.id}`);
+  });
+});
